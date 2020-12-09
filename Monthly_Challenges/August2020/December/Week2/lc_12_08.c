936b10bfb5f8c411089d433f80c613a388813c8b
FlavorlessQuark <mahthaki@gmail.com>
2020-12-09 00:06:06 -0800

    Dec_Day_08

---
+++ Monthly_Challenges/August2020/December/Week2/lc_12_08.c
@@ -0,0 +1,20 @@
+/* Pairs of Songs With Total Durations Divisible by 60 */
+
+long long factorial(int n)
+{
+    return n * (n - 1) / 2;
+}
+
+int numPairsDivisibleBy60(int* time, int timeSize){
+    int count;
+    int mods[60] = {0};
+    count = 0;
+
+    for(int i = 0; i < timeSize; i++)
+        mods[time[i] % 60]++;
+    for (int i = 1; i < 30; i++)
+        count += mods[i] * mods[60 - i];
+    count += factorial(mods[0]);
+    count += factorial(mods[30]);
+    return count;
+}
--- lc_2.c
+++ lc_2.c
@@ -1,4 +1,48 @@
-struct ListNode* addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
 {
-	
+    if (l1 == NULL)
+        return l2;
+    if (l2 == NULL)
+        return l1;
+    if (!l1 && !l2 )
+        return NULL;
+    struct ListNode *ret = NULL, *tmp = NULL;
+
+    tmp = calloc(1,sizeof(struct ListNode));
+    ret = tmp;
+
+    while (1)
+    {
+        if (l1)
+        {
+            tmp->val += l1->val;
+            l1 = l1->next;
+        }
+        if (l2)
+        {
+            tmp->val += l2->val;
+            l2 = l2->next;
+        }
+        if (l1 || l2 || (tmp->val >= 10))
+        {
+            tmp->next = calloc(1, sizeof(struct ListNode));
+            if (tmp->val >= 10)
+            {
+                tmp->next->val = 1;
+                tmp->val -= 10;
+            }
+        }
+         else break ;
+        tmp = tmp->next;
+    }
+    return ret;
 }
