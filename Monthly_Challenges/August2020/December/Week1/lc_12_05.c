da11e0289be5b162e1d1024b153dd40eff1f41a6
FlavorlessQuark <mahthaki@gmail.com>
2020-12-08 20:52:56 -0800

    Dec -Week_1

---
+++ Monthly_Challenges/August2020/December/Week1/lc_12_05.c
@@ -0,0 +1,24 @@
+/* Can place flower */
+
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n){
+    int i;
+    int count;
+
+    i = 0;
+    count = 0;
+
+    if (n == 0)
+        return true;
+    while(i < flowerbedSize)
+    {
+        if (flowerbed[i] == 0 && (((i - 1 < 0) || flowerbed[i - 1] == 0) &&
+                                  (i + 1 >= flowerbedSize || flowerbed[i + 1] == 0)))
+        {
+            flowerbed[i]++;
+             if (++count >= n)
+                return true;
+       }
+        i++;
+    }
+    return false;
+}
