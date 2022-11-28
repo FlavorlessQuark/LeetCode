f61a1f4083481319f3c73943246d2d0e0490ee0a
Jjosephi <31075794+FlavorlessQuark@users.noreply.github.com>
2022-11-27 17:08:08 -0800

    Update lc_0020.py

--- Py_Session/lc_0020.py
+++ Py_Session/lc_0020.py
@@ -3,3 +3,23 @@ class Solution(object):
         while ("()" in s or "[]" in s or '{}' in s):
             s = s.replace("()", "").replace('[]', "").replace('{}', "")
         return len(s) == 0
+
+    def isValid(self, s: str) -> bool:
+        dic = {
+            ')' : '(',
+            ']' : '[',
+            '}' : '{'
+        }
+
+        stack = []
+
+        for x in s:
+            if (x not in dic):
+                stack.append(x)
+            else:
+                # print(stack, x)
+                if (len(stack) == 0 or dic[x] != stack[-1]):
+                    return False
+                else:
+                    stack.pop()
+        return len(stack) == 0
